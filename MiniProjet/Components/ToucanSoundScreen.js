13c923610c66dca0ffdac6ba9d0d3e81f67adad9
Antoine MARIE <antoine28190@gmail.com>
2020-05-04 23:01:21 +0200

    Test du player avec react native track player, appropriation de la doc

--- MiniProjet/Components/ToucanSoundScreen.js
+++ MiniProjet/Components/ToucanSoundScreen.js
@@ -2,14 +2,52 @@
 //Écran d'accueil
 
 import React from 'react'
-import { View, Text, StyleSheet, TouchableOpacity, TouchableHighlight } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity, TouchableHighlight, ViewPropTypes } from 'react-native'
 //Pour le lecteur de son
-import { Player, Recorder, MediaStates } from '@react-native-community/audio-toolkit'
+import TrackPlayer, {
+    useTrackPlayerProgress,
+    usePlaybackState,
+    useTrackPlayerEvents } from 'react-native-track-player'
 //Icône pour la flèche de retour
 import { Ionicons } from '@expo/vector-icons'
 
 //On récupère l'audio du chant
 const getAudio = require("../Audio/Toucan.mp3")
+
+TrackPlayer.setupPlayer().then(() => {
+    // The player is ready to be used
+});
+
+//Barre de progression
+function ProgressBar() {
+    const progress = useTrackPlayerProgress();
+
+    return (
+        <View style={styles.progress}>
+            <View style={{ flex: progress.position, backgroundColor: "red" }} />
+            <View
+                style={{
+                flex: progress.duration - progress.position,
+                backgroundColor: "grey"
+                }}
+            />
+        </View>
+    );
+}
+
+//Bouton de contrôle du player
+function ControlButton({ title, onPress }) {
+    return (
+        <TouchableOpacity style={styles.controlButtonContainer} onPress={onPress}>
+            <Text style={styles.controlButtonText}>{title}</Text>
+        </TouchableOpacity>
+    );
+}
+
+ControlButton.propTypes = {
+    title: PropTypes.string.isRequired,
+    onPress: PropTypes.func.isRequired
+};
 
 class ToucanSoundScreen extends React.Component {
 
@@ -20,55 +58,103 @@ class ToucanSoundScreen extends React.Component {
         };
     }
 
-    /*
-    //On lance l'audio quand la personne clique sur le bouton
-    _onPress() {
-        // Disable button while recording and playing back
-        this.setState({disabled: true});
-
-        // Start recording
-        let rec = new Recorder(getAudio).record();
-
-        // Stop recording after approximately 3 seconds
-        setTimeout(() => {
-            rec.stop((err) => {
-                // NOTE: In a real situation, handle possible errors here
-
-                // Play the file after recording has stopped
-                new Player(getAudio)
-                .play()
-                .on('ended', () => {
-                    // Enable button again after playback finishes
-                    this.setState({disabled: false});
-            });
-            });
-        }, 3000);
-    }
-    */
-
     render() {
 
+        Player.propTypes = {
+            style: ViewPropTypes.style,
+            onNext: PropTypes.func.isRequired,
+            onPrevious: PropTypes.func.isRequired,
+            onTogglePlayback: PropTypes.func.isRequired
+        };
+
+        Player.defaultProps = {
+            style: {}
+        };
+
+        const playbackState = usePlaybackState();
+        const [trackTitle, setTrackTitle] = useState("");
+        const [trackArtwork, setTrackArtwork] = useState();
+        const [trackArtist, setTrackArtist] = useState("");
+        useTrackPlayerEvents(["playback-track-changed"], async event => {
+            if (event.type === TrackPlayer.TrackPlayerEvents.PLAYBACK_TRACK_CHANGED) {
+                const track = await TrackPlayer.getTrack(event.nextTrack);
+                const { title, artist, artwork } = track || {};
+                setTrackTitle(title);
+                setTrackArtist(artist);
+                setTrackArtwork(artwork);
+            }
+        });
+
+        const { style, onNext, onPrevious, onTogglePlayback } = props;
+
+        var middleButtonText = "Play";
+
+        if (
+            playbackState === TrackPlayer.STATE_PLAYING ||
+            playbackState === TrackPlayer.STATE_BUFFERING
+        ) {
+            middleButtonText = "Pause";
+        }
+
         return (
-            <View style={styles.main_container}>
+            <View style={styles.card}>
                 <TouchableOpacity style={styles.back} onPress={() => this.props.navigation.goBack()}>
                     <Ionicons name="ios-arrow-round-back" size={50} color="#FFF"></Ionicons>
                 </TouchableOpacity>
 
-                <TouchableHighlight disabled={this.state.disabled} onPress={() => this._onPress()}>
-                    <Text>
-                        Press me!
-                    </Text>
-                </TouchableHighlight>
+                <ProgressBar />
+                <Text style={styles.title}>{trackTitle}</Text>
+                <Text style={styles.artist}>{trackArtist}</Text>
+                <View style={styles.controls}>
+                    <ControlButton title={"<<"} onPress={onPrevious} />
+                    <ControlButton title={middleButtonText} onPress={onTogglePlayback} />
+                    <ControlButton title={">>"} onPress={onNext} />
+                </View>
             </View>
         )
     }
 }
 
 const styles = StyleSheet.create({
-    main_container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+    card: {
+        width: "80%",
+        elevation: 1,
+        borderRadius: 4,
+        shadowRadius: 2,
+        shadowOpacity: 0.1,
+        alignItems: "center",
+        shadowColor: "black",
+        backgroundColor: "white",
+        shadowOffset: { width: 0, height: 1 }
+    },
+    cover: {
+        width: 140,
+        height: 140,
+        marginTop: 20,
+        backgroundColor: "grey"
+    },
+    progress: {
+        height: 1,
+        width: "90%",
+        marginTop: 10,
+        flexDirection: "row"
+    },
+    title: {
+        marginTop: 10
+    },
+    artist: {
+        fontWeight: "bold"
+    },
+    controls: {
+        marginVertical: 20,
+        flexDirection: "row"
+    },
+    controlButtonContainer: {
+        flex: 1
+    },
+    controlButtonText: {
+        fontSize: 18,
+        textAlign: "center"
     },
     back: {
         position: "absolute",
